bdff96f2148d7648547ff44bff86d909e05f17eb
zaidanlazy <ZaidanMau@gmail.com>
2025-10-15 11:48:34 +0800

    pengetasan lagi

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/rapat', [RapatController::class, 'index']);
-
+// tes 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
      Route::get('/rapat', [RapatController::class, 'index']);
